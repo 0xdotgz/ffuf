2a7c35a6cfaaf076d0ee0e62f7480dce0fb7f3fc
Elliot <78291665+elliotwellick@users.noreply.github.com>
2024-03-18 02:00:12 +0000

    Update simple.go
    changed default useragent

--- pkg/runner/simple.go
+++ pkg/runner/simple.go
@@ -127,7 +127,7 @@ func (r *SimpleRunner) Execute(req *ffuf.Request) (ffuf.Response, error) {
 
 	// set default User-Agent header if not present
 	if _, ok := req.Headers["User-Agent"]; !ok {
-		req.Headers["User-Agent"] = fmt.Sprintf("%s v%s", "Fuzz Faster U Fool", ffuf.Version())
+		req.Headers["User-Agent"] = fmt.Sprintf("Mozilla/5.0 (Windows NT 10.0; rv:123.0) Gecko/20100101 Firefox/123.0")
 	}
 
 	// Handle Go http.Request special cases
